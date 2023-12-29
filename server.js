3ada4b77c71a63bae8dfc5ad63af9472435e3ed5
Gabriel Fernando Kraetzer <gabriel.kraetzer@philips.com>
2023-12-29 11:24:47 -0300

    remove external dependencies, keep just node.js

--- server.js
+++ server.js
@@ -1,6 +1,44 @@
-const connect = require('connect');
-const serveStatic = require('serve-static');
+const fs = require("fs")
+const http = require("http");
+const path = require("path");
 
-connect()
-    .use(serveStatic('./'))
-    .listen(666, () => console.log('Server running on 666...'));
+const PORT = 666;
+
+const MIME_TYPES = {
+    default: "application/octet-stream",
+    html: "text/html; charset=UTF-8",
+    js: "application/javascript",
+    css: "text/css"
+};
+
+const STATIC_PATH = path.join(process.cwd(), "./");
+
+const toBool = [() => true, () => false];
+
+const prepareFile = async (url) => {
+    const paths = [STATIC_PATH, url];
+    if (url.endsWith("/")) {
+        paths.push("index.html");
+    }
+    const filePath = path.join(...paths);
+    const pathTraversal = !filePath.startsWith(STATIC_PATH);
+    const exists = await fs.promises.access(filePath).then(...toBool);
+    const found = !pathTraversal && exists;
+    const streamPath = found ? filePath : STATIC_PATH + "/404.html";
+    const ext = path.extname(streamPath).substring(1).toLowerCase();
+    const stream = fs.createReadStream(streamPath);
+    return { found, ext, stream };
+};
+
+http
+    .createServer(async (req, res) => {
+        const file = await prepareFile(req.url);
+        const statusCode = file.found ? 200 : 404;
+        const mimeType = MIME_TYPES[file.ext] || MIME_TYPES.default;
+        res.writeHead(statusCode, { "Content-Type": mimeType });
+        file.stream.pipe(res);
+        console.log(`${req.method} ${req.url} ${statusCode}`);
+    })
+    .listen(PORT);
+
+console.log(`Server running at http://127.0.0.1:${PORT}/`);
