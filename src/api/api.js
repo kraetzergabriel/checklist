e597cf290abf2cb750b75c69d09c8702d3c55109
Gabriel Kraetzer <gabriel.kraetzer@gmail.com>
2025-04-27 23:33:12 -0300

    feat(api.js): improve data access

---
+++ src/api/api.js
@@ -0,0 +1,73 @@
+// TODO criar serializacao e deserializacao do arquivo data.json, este substituira o uso dos cookies
+
+// fetch('?', {})
+//     .then((response) => {
+//         if (!response.ok) {
+//             throw new Error('Fetch data failed');
+//         }
+//         return response.json();
+//     }).catch((error) => {
+//         console.log(error);
+//     })
+
+class ApiFetch {
+    constructor() {
+// TODO nothing yet
+    }
+
+    async save(json) {
+        try {
+            const response = await fetch('http://localhost:8081/', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(json)
+            });
+            if (!response.ok) {
+                throw new Error('error')
+            }
+
+            console.log(`save successful ${response}`)
+        } catch (error) {
+            console.log(`save error ${error}`)
+        }
+    }
+
+    async get(headers = {}) {
+        try {
+            const response = fetch('./data.json', headers);
+            if (!response.ok) {
+                throw new Error('error during fetching data.json');
+            }
+            return await response.json();
+        } catch (error) {
+            console.log(error);
+        }
+
+        return null;
+    }
+
+    async getChecklistData() {
+        const data = this.get();
+        if (data.checklist && data.checklist.lenght > 0) {
+            return data.checklist;
+        }
+    }
+
+    saveChecklistData() {
+    }
+
+    getCardData() {
+        const data = this.get();
+        if (data.card && data.card.lenght > 0) {
+            return data.card;
+        }
+    }
+
+    saveCardData() {
+    }
+
+}
+
+export const api = new ApiFetch();
